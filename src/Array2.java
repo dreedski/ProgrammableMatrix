bc46a4acb1fb197c408726a01858f3b8e43554eb
dreedski <dreedski@hotmail.com>
2021-04-07 23:19:48 -0500

    Update Array2.java

--- src/Array2.java
+++ src/Array2.java
@@ -3,10 +3,9 @@ import java.util.*;
 public class Array2 {
     MainProg main1 = new MainProg();
     Scanner in = new Scanner(System.in);
-    //private ArrayList[][];
+    private String [][] newArray;
     private int row;
     private int column;
-    String [][] newArray;
 
 
 
@@ -18,24 +17,56 @@ public class Array2 {
 
     public void getElement() {
         Scanner in = new Scanner(System.in);
-        System.out.println("What row is the element you would like to get in? (Must be under " + row + ")");
-        int userRow = in.nextInt();
-        System.out.println("What column is the element you like to get in? (Must be under " + column + ")");
-        int userCol = in.nextInt();
+        int userRow;
+        int userCol;
+        //get row for the element user wants
+        do {
+            System.out.println("What row is the element you would like to get in? (Must be under " + row + ")");
+            userRow = in.nextInt();
+            if (userRow > row) {
+                System.out.println("Your number must be less than " + row);
+            }
+        }while(userRow > row);
+        //get column for the element the user wants
+        do {
+            System.out.println("What column is the element you like to get in? (Must be under " + column + ")");
+            userCol = in.nextInt();
+            if (userCol > column) {
+                System.out.println("Your number must be less than " + column);
+            }
+        }while(userCol > column);
+        //print out the row/column the user entered for the element they want
         System.out.println("You have entered: " + "\n" +
                 "Row " + userRow + "\n" +
                 "Column " + userCol);
+        //get the element the user wants
         String getElement = newArray[userRow-1][userCol-1];
         System.out.println(getElement);
     }
 
     public void setElement() {
-        System.out.println("What row would you like your element in? (Must be under " + row + ")");
-        int userRow = in.nextInt();
+        int userRow;
+        int userCol;
+        //get row for element to set from user
+        do {
+            System.out.println("What row would you like your element in? (Must be under " + row + ")");
+            userRow = in.nextInt();
+            if (userRow > row) {
+                System.out.println("Your number must be less than " + row);
+            }
+        }while(userRow > row);
         System.out.println("What column would you like your element in? (Must be under " + column + ")");
-        int userCol = in.nextInt();
+        //get column for element to set from user
+        do {
+            userCol = in.nextInt();
+            if (userCol > column) {
+                System.out.println("Your number must be less than " + column);
+            }
+        }while(userCol > column);
+        //get char from user they want to set
         System.out.println("What character would you like the element to be?");
         String userChar = in.next();
+        //print out the new row, column, and char that user wants
         System.out.println("You have entered: " + "\n" +
                 "Row " + userRow + "\n" +
                 "Column " + userCol + "\n" +
@@ -45,10 +76,24 @@ public class Array2 {
 
     public void clearElement() {
         Scanner in = new Scanner(System.in);
-        System.out.println("What row is the element you would like empty? (Must be under " + row + ")");
-        int userRow = in.nextInt();
-        System.out.println("What column is the element you like to empty? (Must be under " + column + ")");
-        int userCol = in.nextInt();
+        int userRow;
+        int userCol;
+        //get element row to remove from user
+        do{
+            System.out.println("What row is the element you would like empty? (Must be under " + row + ")");
+            userRow = in.nextInt();
+            if (userRow > row) {
+                System.out.println("Your number must be less than " + row);
+            }
+        }while(userRow > row);
+        //get element column to remove from user
+        do {
+            System.out.println("What column is the element you like to empty? (Must be under " + column + ")");
+            userCol = in.nextInt();
+            if (userCol > column) {
+                System.out.println("Your number must be less than " + column);
+            }
+        }while(userCol > column);
         System.out.println("You have entered: " + "\n" +
                 "Row " + userRow + "\n" +
                 "Column " + userCol);
@@ -63,8 +108,8 @@ public class Array2 {
             for (int j = 0; j < newArray[i].length; j++) {
                 System.out.print(newArray[i][j]);
             }
-            for (String[] row: newArray)
-                Arrays.fill(row, " ");
+            //for (String[] row: newArray)
+            //    Arrays.fill(row, " ");
             System.out.println();
         }
     }
