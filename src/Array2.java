da169788d3bb6d6f7ca9a58415d7a52cacd6e34b
dreedski <dreedski@hotmail.com>
2021-04-07 19:29:39 -0500

    Initial commit

---
+++ src/Array2.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+public class Array2 {
+    MainProg main1 = new MainProg();
+    Scanner in = new Scanner(System.in);
+    //private ArrayList[][];
+    private int row;
+    private int column;
+    String [][] newArray;
+
+
+
+    public Array2(int row, int column, String[][] newArray) {
+        this.row = row;
+        this.column = column;
+        this.newArray = newArray;
+    }
+
+    public void getElement() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("What row is the element you would like to get in? (Must be under " + row + ")");
+        int userRow = in.nextInt();
+        System.out.println("What column is the element you like to get in? (Must be under " + column + ")");
+        int userCol = in.nextInt();
+        System.out.println("You have entered: " + "\n" +
+                "Row " + userRow + "\n" +
+                "Column " + userCol);
+        String getElement = newArray[userRow][userCol];
+    }
+
+    public void setElement() {
+        System.out.println("What row would you like your element in? (Must be under " + row + ")");
+        int userRow = in.nextInt();
+        System.out.println("What column would you like your element in? (Must be under " + column + ")");
+        int userCol = in.nextInt();
+        System.out.println("What character would you like the element to be?");
+        String userChar = in.next();
+        System.out.println("You have entered: " + "\n" +
+                "Row " + userRow + "\n" +
+                "Column " + userCol + "\n" +
+                "Char to be entered: " + userChar);
+        newArray[userRow][userCol] = String.valueOf(userChar);
+    }
+
+    public void clearElement() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("What row is the element you would like empty? (Must be under " + row + ")");
+        int userRow = in.nextInt();
+        System.out.println("What column is the element you like to empty? (Must be under " + column + ")");
+        int userCol = in.nextInt();
+        System.out.println("You have entered: " + "\n" +
+                "Row " + userRow + "\n" +
+                "Column " + userCol);
+
+    }
+
+    public void printMatrix() {
+        Scanner in = new Scanner(System.in);
+        //String result = " ";
+        System.out.println("The array is: \n");
+        for (int i = 0; i < newArray.length; i++) {
+            for (int j = 0; j < newArray[i].length; j++) {
+                System.out.print(newArray[i][j]);
+            }
+            for (String[] row: newArray)
+                Arrays.fill(row, "");
+            System.out.println();
+        }
+    }
+
+    public void quitProgram() {
+        System.out.println("The system will now exit! BYE!!!");
+        System.exit(0);
+    }
+}
---
+++ src/MainProg.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class MainProg {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("How many rows do you want for your matrix?  ");
+        int row = in.nextInt();
+
+        System.out.print("How many columns do you want for your matrix?  ");
+        int column = in.nextInt();
+        String[][] newArray = new String[row][column];
+
+
+        Array2 twoDArray = new Array2(row, column, newArray); //calling my class
+
+
+        System.out.println("If you would like to set an, element press S: " + "\n" +
+                "If you would like to set an element, press G" + "\n" +
+                "If you would like to empty an element, press E" + "\n" +
+                "If you would like to print an element, press P" + "\n" +
+                "If you would like to quit, press Q");
+        String userInput = in.next();
+       // while (!userInput.equalsIgnoreCase("q")){
+            if (userInput.equalsIgnoreCase("S")) {
+                twoDArray.setElement();
+            } else if (userInput.equalsIgnoreCase("G")) {
+                twoDArray.getElement();
+            } else if (userInput.equalsIgnoreCase("E")) {
+                twoDArray.clearElement();
+            } else if (userInput.equalsIgnoreCase("P")) {
+                twoDArray.printMatrix();
+                //you will do you toString here
+            } else if (userInput.equalsIgnoreCase("Q")) {
+                //this will quit the program
+                twoDArray.quitProgram();
+            }
+        } //while(!userInput.equalsIgnoreCase("q"));
+}
